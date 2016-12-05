c9928758b1fdd77d8fc21d5b92934f876a2ab350
xixilide <754396775@qq.com>
2016-12-05 10:35:54 +0800

    xixilide

--- src/Spreadtrum/Spreadtrum.js
+++ src/Spreadtrum/Spreadtrum.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react'
-
+import Pichover from './pichover'
 class Spreadtrum extends React.Component {
   render () {
     return(
       <div>
+        <Pichover />
       Spreadtrum
       </div>
     )
