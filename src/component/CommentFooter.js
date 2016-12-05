fa7f202c71223b28cd27b072087e33d715953cae
jingli <351201853@qq.com>
2016-12-05 11:39:10 +0800

    jingli

---
+++ src/component/CommentFooter.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react'
+
+class CommentFooter extends React.Component {
+  render () {
+    return(
+      <div className='commentfooter-wrap'>
+        <div className='commentfooter-comment'>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CommentFooter;
