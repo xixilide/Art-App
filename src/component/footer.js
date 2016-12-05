91b53ca7dc267f106b1d6cf4a4bea0d185953ca9
xixilide <754396775@qq.com>
2016-12-05 14:36:56 +0800

    xixilide-home

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,16 +1,23 @@
 import React, { PropTypes } from 'react'
 import {  Link} from 'react-router';
-
+import Img1 from '../images/littlepic/9.png'
+import Img2 from '../images/littlepic/7.png'
+import Img3 from '../images/littlepic/3.png'
+import Img4 from '../images/littlepic/5.png'
 class Footer extends React.Component {
   render () {
+    let styles = {
+      img:{
+        width:"12vw",
+        height:'8vh'
+      }
+    }
   return(
-    <div>
-      <ol className="nav-footer">
-        <li><Link to="/login" activeStyle={{background:'teal'}} onlyActiveOnIndex={true}>展览</Link></li>
-        <li><Link to="/recommend" activeStyle={{background:'teal'}}>推荐</Link></li>
-        <li><Link to="/release" activeStyle={{background:'teal'}}>发布</Link></li>
-        <li><Link to="/person" activeStyle={{background:'teal'}}>个人</Link></li>
-      </ol>
+    <div className="nav-footer">
+        <Link to="/login"  onlyActiveOnIndex={true} ><img src={Img1} style={styles.img}/></Link>
+        <Link to="/recommend"> <img src={Img2} style={styles.img}/></Link>
+        <Link to="/release"> <img src={Img3} style={styles.img}/></Link>
+        <Link to="/person"> <img src={Img4} style={styles.img}/></Link>
     </div>
   )
   }
