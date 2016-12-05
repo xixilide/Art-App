54c0bdde5db638ab9cd5856dbf4ab49a9e3d84be
rong12345 <1165895564@qq.com>
2016-12-05 14:39:39 +0800

    jiangrong

--- src/component/footer.js
+++ src/component/footer.js
@@ -5,12 +5,12 @@ class Footer extends React.Component {
   render () {
   return(
     <div>
-      <ol className="nav-footer">
+      <ul className="nav-footer">
         <li><Link to="/login" activeStyle={{background:'teal'}} onlyActiveOnIndex={true}>展览</Link></li>
         <li><Link to="/recommend" activeStyle={{background:'teal'}}>推荐</Link></li>
         <li><Link to="/release" activeStyle={{background:'teal'}}>发布</Link></li>
         <li><Link to="/person" activeStyle={{background:'teal'}}>个人</Link></li>
-      </ol>
+      </ul>
     </div>
   )
   }
