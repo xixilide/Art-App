11f665ceb48db8851fbb46f7c7b3013b4a0d156d
xixilide <754396775@qq.com>
2016-12-06 14:17:49 +0800

    xixilide

--- src/Spreadtrum/classify/huihua.js
+++ src/Spreadtrum/classify/huihua.js
@@ -1,14 +1,19 @@
 import React, { PropTypes } from 'react'
-
-class Release extends React.Component {
+import Recommendzhan from '../../component/footer'
+import Footer from '../../Recommend/recommendzhan'
+class ClaDraw extends React.Component {
   render () {
   return(
     <div className="claH">
-
+      <div className="kong"></div>
+      <div className="clswen">
+        <Recommendzhan />
+        </div>
+        <Footer />
     </div>
 
   )
   }
 }
 
-export default Release;
+export default ClaDraw;
